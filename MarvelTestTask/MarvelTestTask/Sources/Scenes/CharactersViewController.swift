71a344c3282174aa2f03cacba6b69f8c60d11674
DaniilSmirn0v <d1n.sm1rnv@gmail.com>
2022-07-29 19:36:54 +0300

    Prepare project to work.

---
+++ MarvelTestTask/MarvelTestTask/Sources/Scenes/CharactersViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CharactersViewController.swift
+//  MarvelTestTask
+//
+//  Created by Даниил Смирнов on 29.07.2022.
+//
+
+import Foundation
+import UIKit
+
+class CharactersViewController: UIViewController {
+    
+    
+    //MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+    
+    
+}
